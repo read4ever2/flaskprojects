f274fb5230f24cd1ce578e78ad3bd277ee6c0fcc
willfeighner <read4ever2@gmail.com>
2021-04-24 15:50:57 -0500

    commit

--- hello_sdev.py
+++ hello_sdev.py
@@ -9,6 +9,26 @@ def index():
     return show_hello()
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+
+
+        password = request.form["password"]
+        error = None
+        if not username:
+            error = "Username is required."
+
+        elif not password:
+            error = "Password is required."
+        flash(error)
+        # TODO: Encrypt and Store credentials
+        if error == None:
+            return redirect(url_for("hello"))
+    return render_template('register.html')
+
+
 # Can call functions as part of the returns
 def show_hello():
     return 'Hello, UMGC SDEV Students!'
